b1e1dcfa6487b6c054f8007c7a3ca30ec4f3425e
StefanKwinta <38467124+StefanKwinta@users.noreply.github.com>
2021-02-19 21:30:23 +0100

    branches tests

--- src/main/java/com/springprojects/mailmanager/MailManagerApplication.java
+++ src/main/java/com/springprojects/mailmanager/MailManagerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MailManagerApplication {
-
+//    branche żeby sprawdzić
     public static void main(String[] args) {
         SpringApplication.run(MailManagerApplication.class, args);
     }
